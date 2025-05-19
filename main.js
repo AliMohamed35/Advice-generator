1e252555ffd1bb7046ec43526eff50c61888816d
Ali Mohamed <alimohamed351@outlook.com>
2025-05-19 17:53:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const endPoint = `https://api.adviceslip.com/advice`;
+
+async function fetchAdvices() {
+    try {
+        let response = await fetch(endPoint);
+
+        if (!response.ok) {
+            throw new Error('failed to fetch')
+        } else {
+            let data = await response.json();
+            generateAdvice(data)
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function generateAdvice(data) {
+    let advice = document.getElementById('advice');
+    let advice_id = document.getElementById('advice-id');
+    advice.innerHTML = data.slip.advice;
+    advice_id.innerHTML = data.slip.id;
+}
+
+fetchAdvices();
